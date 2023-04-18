650a8884199716586233c791ac3f9cf2c82457c7
Раб <reg24885@gmail.com>
2023-04-18 10:56:02 +0300

    v0.3

--- GUI.py
+++ GUI.py
@@ -5,78 +5,8 @@ from ReJ import AvtoJ
 import ListOfDisciplines
 from importlib import reload
 from threading import Thread
-from datetime import *
+from StudentFrame import StudentFrame
 
-
-
-'''Форма для проверки явки'''
-
-
-class StudentFrame(CTkScrollableFrame):
-    def __init__(self, master, session, dics, prac, **kwargs):
-        super().__init__(master, **kwargs)
-        self.session = session
-        self.disc = dics
-        self.prac = prac
-        self.rows = session.student_rows(dics['id_group'], dics['subject_id'], prac=self.prac,
-                                         date_from=datetime.today().strftime('%d.%m.%Y'))['rows']
-        self.disc2 = None
-        self.rows2 = None
-        self.value_combobox = ['', 'Н', 'Б', 'У', 'О']
-        self.combo = []
-        self.label = []
-        for j, i in enumerate(self.rows):
-            self.student_lesson(i, j)
-
-    def add_v_group(self, dics):
-        self.disc2 = dics
-        self.rows2 = self.session.student_rows(dics['id_group'], dics['subject_id'], prac=self.prac,
-                                         date_from=datetime.today().strftime('%d.%m.%Y'))['rows']
-        for j, i in enumerate(self.rows2):
-            self.student_lesson(i, j+len(self.rows))
-
-    '''Метод создания явки на 1 студента'''
-
-    def student_lesson(self, student, j):
-        self.label.append(CTkLabel(self, text=student['student_name']))
-        self.label[-1].grid(row=j, column=0, padx=20, pady=5)
-        self.combo.append([])
-        for i, n in enumerate(student['lessons']):
-            self.combo[-1].append(
-                CTkOptionMenu(self, values=self.value_combobox, width=50, command=lambda x: self.p(j, x)))
-            self.combo[-1][-1].grid(row=j, column=i + 1, padx=5, pady=5)
-            self.combo[-1][-1].set(n['attendance']['value'])
-
-    '''Метод проставления значений студенту до конца занятий'''
-
-    def p(self, j, x):
-        if x != 'О':
-            set_box = False
-            for box in self.combo[j]:
-                if box.get() == x:
-                    set_box = True
-                elif set_box:
-                    box.set(x)
-
-    '''Выставление явки в журнал'''
-
-    def turnout(self):
-        n = len(self.rows)
-        for id_student, student in enumerate(self.combo[:n]):
-            for id_lesson, lesson in enumerate(student):
-                if lesson.get() != '':
-                    self.session.setting_turnout(self.disc['id_group'], self.disc['subject_id'],
-                                                 self.rows[id_student]['student_id'],
-                                                 self.rows[id_student]['lessons'][id_lesson]['id'], lesson.get(),
-                                                 prac=self.prac)
-        if self.rows2 is not None:
-            for id_student, student in enumerate(self.combo[n:]):
-                for id_lesson, lesson in enumerate(student):
-                    if lesson.get() != '':
-                        self.session.setting_turnout(self.disc2['id_group'], self.disc2['subject_id'],
-                                                     self.rows2[id_student]['student_id'],
-                                                     self.rows2[id_student]['lessons'][id_lesson]['id'], lesson.get(),
-                                                     prac=self.prac)
 
 
 '''Форма процесса загрузки данных'''
@@ -229,11 +159,11 @@ class LoginForm(CTkToplevel):
 class APP(CTk):
     def __init__(self):
         super().__init__()
-        set_appearance_mode("System")
+        set_appearance_mode("Dark")
         set_default_color_theme('blue')
         self.session = AvtoJ()
-        self.geometry('1280x720')
-        x = (self.winfo_screenwidth() - 1280) / 2
+        self.geometry('555x720')
+        x = (self.winfo_screenwidth() - 555) / 2
         y = (self.winfo_screenheight() - 720) / 2
         self.geometry("+%d+%d" % (x, y))
         self.resizable(False, False)
@@ -265,15 +195,15 @@ class APP(CTk):
         pr = self.tab.frame_pr.get_check_group()
         if len(tr) + len(pr) == 1:
             if len(tr) == 1:
-                self.studen_frame = StudentFrame(self, self.session, ListOfDisciplines.Theory[tr[0]], '', width=610,
+                self.studen_frame = StudentFrame(self.session, ListOfDisciplines.Theory[tr[0]], '', width=1280,
                                                  height=500)
                 self.studen_frame.add_v_group(ListOfDisciplines.Theory[tr[0]+1])
             elif len(pr) == 1:
-                self.studen_frame = StudentFrame(self, self.session, ListOfDisciplines.Practice[pr[0]], '1', width=610,
+                self.studen_frame = StudentFrame(self.session, ListOfDisciplines.Practice[pr[0]], '1', width=1280,
                                                  height=500)
                 if ListOfDisciplines.Practice[pr[0]]['name'].find('/2') != -1:
                     self.studen_frame.add_v_group(ListOfDisciplines.Practice[pr[0] + 1])
-            self.studen_frame.grid(row=0, column=3, pady=10, padx=10, columnspan=3)
+            self.studen_frame.grid(row=0, column=0, pady=10, padx=10, columnspan=3)
         else:
             showerror(title="Ошибка", message="Надо выбрать 1 группу")
 
@@ -293,8 +223,7 @@ class APP(CTk):
         button.grid(row=2, column=1, pady=10, padx=10)
         button = CTkButton(self, text='Проверить явку', command=lambda: self.create_student_frame())
         button.grid(row=1, column=2, pady=10, padx=10)
-        button = CTkButton(self, text='Сохранить явку', command=lambda: self.studen_frame.turnout())
-        button.grid(row=2, column=2, pady=10, padx=10)
+
 
 
 if __name__ == '__main__':
---
+++ StudentFrame.py
@@ -0,0 +1,76 @@
+from customtkinter import *
+from datetime import *
+
+'''Форма для проверки явки'''
+
+
+class StudentFrame(CTkScrollableFrame):
+    def __init__(self, session, dics, prac, **kwargs):
+        self.root = CTkToplevel()
+        super().__init__(self.root, **kwargs)
+        self.session = session
+        self.disc = dics
+        self.prac = prac
+        self.rows = session.student_rows(dics['id_group'], dics['subject_id'], prac=self.prac,
+                                         date_from=datetime.today().strftime('%d.%m.%Y'))['rows']
+        self.disc2 = None
+        self.rows2 = None
+        self.value_combobox = ['', 'Н', 'Б', 'У', 'О']
+        self.combo = []
+        self.label = []
+        for j, i in enumerate(self.rows):
+            self.student_lesson(i, j)
+        button = CTkButton(self.root, text='Сохранить явку', command=lambda: self.turnout())
+        button.grid(row=2, column=0, pady=10, padx=10)
+        self.root.focus_set()
+
+    def add_v_group(self, dics):
+        self.disc2 = dics
+        self.rows2 = self.session.student_rows(dics['id_group'], dics['subject_id'], prac=self.prac,
+                                         date_from=datetime.today().strftime('%d.%m.%Y'))['rows']
+        for j, i in enumerate(self.rows2):
+            self.student_lesson(i, j+len(self.rows))
+
+    '''Метод создания явки на 1 студента'''
+
+    def student_lesson(self, student, j):
+        self.label.append(CTkLabel(self, text=student['student_name']))
+        self.label[-1].grid(row=j, column=0, padx=20, pady=5)
+        self.combo.append([])
+        for i, n in enumerate(student['lessons']):
+            self.combo[-1].append(
+                CTkOptionMenu(self, values=self.value_combobox, width=50, command=lambda x: self.p(j, x)))
+            self.combo[-1][-1].grid(row=j, column=i + 1, padx=5, pady=5)
+            self.combo[-1][-1].set(n['attendance']['value'])
+
+
+    '''Метод проставления значений студенту до конца занятий'''
+
+    def p(self, j, x):
+        if x != 'О':
+            set_box = False
+            for box in self.combo[j]:
+                if box.get() == x:
+                    set_box = True
+                elif set_box:
+                    box.set(x)
+
+    '''Выставление явки в журнал'''
+
+    def turnout(self):
+        n = len(self.rows)
+        for id_student, student in enumerate(self.combo[:n]):
+            for id_lesson, lesson in enumerate(student):
+                #if lesson.get() != '':
+                    self.session.setting_turnout(self.disc['id_group'], self.disc['subject_id'],
+                                                 self.rows[id_student]['student_id'],
+                                                 self.rows[id_student]['lessons'][id_lesson]['id'], lesson.get(),
+                                                 prac=self.prac)
+        if self.rows2 is not None:
+            for id_student, student in enumerate(self.combo[n:]):
+                for id_lesson, lesson in enumerate(student):
+                    #if lesson.get() != '':
+                        self.session.setting_turnout(self.disc2['id_group'], self.disc2['subject_id'],
+                                                     self.rows2[id_student]['student_id'],
+                                                     self.rows2[id_student]['lessons'][id_lesson]['id'], lesson.get(),
+                                                     prac=self.prac)
