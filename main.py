f2148be5bf96495c748580e511087376935b286a
Раб <reg24885@gmail.com>
2023-04-20 14:53:14 +0300

    v 0.4

--- main.py
+++ main.py
@@ -17,14 +17,30 @@ from datetime import *
 class Calendar(CTk):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.canvas = Canvas(self)
+        self.canvas = Canvas(self, width=140, height=100)
         self.canvas.grid(row=0, column=2, pady=10, padx=10)
-        self.mount = '01'
-        self.date = datetime.strptime('01/01/2023', '%d/%m/%y')
-
+        self.date_mass = [4, 23]
+        self.size = 20
+        self.create_days()
 
 
     def create_days(self):
         n = 0
-        self.canvas.create_rectangle(n,)
+        for i in range(1, 32):
+            try:
+                date = datetime.strptime(f'{i}/{self.date_mass[0]}/{self.date_mass[1]}', '%d/%m/%y')
+                w = 6 if (d := int(datetime.strftime(date, '%w'))) == 0 else d-1
+                self.canvas.create_rectangle(w * self.size, n * self.size,
+                                             (1+w) * self.size, (n+1) * self.size,
+                                             fill='#cccccc' if w < 5 else '#999999', tags=f'{i}')
+                self.canvas.create_text(((w * self.size) + ((1 + w) * self.size))/2,
+                                        ((n * self.size)+(n + 1) * self.size)/2, text=f'{i}',
+                                        fill='black')
+                if w == 6:
+                    n += 1
+            except ValueError:
+                break
 
+
+s = Calendar()
+s.mainloop()
