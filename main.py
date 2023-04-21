5e6eb78fd8754b7850e2db272300dcf6692d5e1a
Раб <reg24885@gmail.com>
2023-04-21 10:47:29 +0300

    v 0.5

--- main.py
+++ main.py
@@ -1,16 +1,2 @@
-from typing import Any
-
-import GUI
-import ReJ
-
-'''s = ReJ.AvtoJ()
-s.set_cookie('ssuz_sessionid=zjehaw1kic0m54f190m19wrqsf655gcg')
-#s.create_score_pole('4078', '{"subject_id": 2484, "sub_group_id": 13511}', '3381956', prac='1')
-print(s.show_score_pole('4078', '{"subject_id": 2484, "sub_group_id": 13511}', '3381956', prac='1')['rows'][0]['id'])
-#s.expose_score('4078', '{"subject_id": 2484, "sub_group_id": 13511}', '3381956', '1366884', '5', '61657')'''
-
-
-
-
-
+'''test bild'''
 
