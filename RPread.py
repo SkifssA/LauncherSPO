5586352844613cfc7101070cd4963d9f5b431f63
SkifssA <reg24885@gmail.com>
2023-05-12 19:22:13 +0300

    v 0.5

--- RPread.py
+++ RPread.py
@@ -1,5 +1,4 @@
 '''
-Создание таблицы(?) для ввода темы и кол-во часов
 Выбор групп у которых рп одинаковые
 Сохранение всего(?)
 
@@ -9,37 +8,50 @@
 from customtkinter import *
 from tkinter import *
 from tkinter import ttk
+from tkinter.messagebox import showerror
 
 class Create_RP(CTkFrame):
-    def __init__(self, master):
+    def __init__(self, master, *disc):
         super().__init__(master)
-        self.set = ttk.Treeview(self)
-        self.set.grid()
-        self.set['columns'] = ('t', 'c')
-        self.set.column("#0", width=0, stretch=NO)
-        self.set.column("t", anchor=CENTER, width=500)
-        self.set.column("c", anchor=CENTER, width=100)
 
-        self.set.heading("#0", text="", anchor=CENTER)
-        self.set.heading("t", text="Тема", anchor=CENTER)
-        self.set.heading("c", text="Кол-во часов", anchor=CENTER)
+        self.id_group = set(x['id_group'] for x in disc)
 
-        self.entry = [CTkEntry(self), CTkEntry(self)]
+        self.tabl = ttk.Treeview(self)
+        self.tabl.grid()
+        self.tabl['columns'] = ('t', 'c')
+        self.tabl.column("#0", width=0, stretch=NO)
+        self.tabl.column("t", anchor=CENTER, width=500)
+        self.tabl.column("c", anchor=CENTER, width=100)
+
+        self.tabl.heading("#0", text="", anchor=CENTER)
+        self.tabl.heading("t", text="Тема", anchor=CENTER)
+        self.tabl.heading("c", text="Кол-во часов", anchor=CENTER)
+
+        self.entry = [CTkEntry(self, width=500), CTkEntry(self)]
         self.entry[0].grid()
         self.entry[1].grid()
 
         self.count = 0
 
-        self.button_save = CTkButton(self, command=self.save_in_table)
+        self.button_save = CTkButton(self,text='Добавить тему', command=self.save_in_table)
+        self.button_save.grid()
+
+        self.button_save = CTkButton(self, text='Сохранить файл', command=self.save_file)
         self.button_save.grid()
 
     def save_in_table(self):
-        if self.entry[0].get() != '' and self.entry[1].get() != '':
-            self.set.insert(parent='', index='end', iid=self.count, text='',
+        if self.entry[0].get() != '' and self.entry[1].get() != '' and self.entry[1].get().isdigit():
+            self.tabl.insert(parent='', index='end', iid=self.count, text='',
                            values=(self.entry[0].get().replace('\n', ' '), self.entry[1].get()))
             self.entry[0].delete(0, END)
             self.entry[1].delete(0, END)
             self.count += 1
+        else:
+            showerror(title="Ошибка", message="Неверные значения")
+
+    def save_file(self):
+        for iid in self.tabl.get_children():
+            print(self.tabl.item(iid)['values'])
 
 
 
