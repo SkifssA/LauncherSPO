fe7d712c15610bf4f435bd36ef16a87022de289d
Раб <reg24885@gmail.com>
2023-05-12 13:11:18 +0300

    test bild

--- RPread.py
+++ RPread.py
@@ -24,11 +24,22 @@ class Create_RP(CTkFrame):
         self.set.heading("t", text="Тема", anchor=CENTER)
         self.set.heading("c", text="Кол-во часов", anchor=CENTER)
 
-        self.set.insert(parent='', index='end', iid=0, text='',
-                   values=('Основы ООП', '4'))
+        self.entry = [CTkEntry(self), CTkEntry(self)]
+        self.entry[0].grid()
+        self.entry[1].grid()
 
+        self.count = 0
 
+        self.button_save = CTkButton(self, command=self.save_in_table)
+        self.button_save.grid()
 
+    def save_in_table(self):
+        if self.entry[0].get() != '' and self.entry[1].get() != '':
+            self.set.insert(parent='', index='end', iid=self.count, text='',
+                           values=(self.entry[0].get().replace('\n', ' '), self.entry[1].get()))
+            self.entry[0].delete(0, END)
+            self.entry[1].delete(0, END)
+            self.count += 1
 
 
 
